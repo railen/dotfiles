2d94636832342e88a345c7745b71f0f337cb2466
Alexandr V. Lebedev <a.v.lebedev@list.ru>
2016-03-24 13:07:14 +0300

    Added packages installation script

---
+++ install-packages.sh
@@ -0,0 +1,29 @@
+#/bin/sh
+apt-get install --no-install-recommends \
+    mc awesome awesome-extra feh \
+    xorg xserver-xorg-video-ati xserver-xorg-input-mouse \
+    firefox thunderbird \
+    firefox-locale-ru thunderbird-locale-ru \
+    rxvt-unicode \
+    vim vim-gtk \
+    xfonts-terminus \
+    lm-sensors \
+    xserver-xephyr \
+    git mercurial subversion \
+    python-gtk2 python-cairo python-gobject-2 \
+    xscreensaver xscreensaver-gl xscreensaver-gl-extra xscreensaver-screensaver-bsod xscreensaver-screensaver-dizzy xscreensaver-screensaver-webcollage \
+    vim-runtime vim-common vim-gui-common vim-tiny curl exuberant-ctags \
+    ttf-mscorefonts-installer \
+    polipo pdnsd \
+    encfs \
+    flashplugin-installer \
+    pulseaudio alsa-base alsa-utils pulseaudio-module-x11 \
+    kbdd htop fonts-liberation \
+    libqt4-dev qtcreator \
+    gcc make g++ \
+    shorewall gdb \
+    thunar thunar-archive-plugin thunar-volman notification-daemon \
+    apache2 mysql-client mysql-server mysql-workbench ttf-bitstream-vera \
+    keepass2 xdg-utils remmina remmina-plugin-rdp \
+    eric \
+    gtk2-engines
