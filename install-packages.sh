40b12c52edb79f2f00fdb1b88d564ea2c5f7a6a5
Alexandr V. Lebedev <a.v.lebedev@list.ru>
2017-05-30 15:41:57 +0300

    Added full path to package list

--- install-packages.sh
+++ install-packages.sh
@@ -1,2 +1,19 @@
 #/bin/sh
-apt install -y --no-install-recommends $(grep -vE "^\s*#" package-list  | tr "\n" " ")
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+APT_FLAGS=-sy --no-insall-recommends
+echo apt install $APT_FLAGS $(grep -vE "^\s*#" $DIR/package-list  | tr "\n" " ")
+
+# Heroku repo and key
+add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+curl -L https://cli-assets.heroku.com/apt/release.key | apt-key add -
+
+# Dropbox’s repository key
+apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+
+# Add Dropbox’s repository
+add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
+
+# Update and install dropbox and heroku
+apt update
+apt install $APT_FLAGS nautilus-dropbox heroku
+
