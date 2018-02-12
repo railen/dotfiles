f33ba962ca989d0264706f203237ccf51537e716
Alexandr V. Lebedev <a.v.lebedev@list.ru>
2018-02-12 15:56:51 +0300

    FZF added

--- install.sh
+++ install.sh
@@ -11,4 +11,7 @@ ln -s ~/.dotfiles/.mutt ~/.mutt
 /usr/bin/git clone https://github.com/joonty/vdebug.git  ~/.vim/bundle/pristine/vdebug
 /usr/bin/git clone https://github.com/vim-scripts/DoxygenToolkit.vim.git ~/.vim/bundle/pristine/DoxygenToolkit.vim
 /usr/bin/git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/pristine/ctrlp.vim
+/usr/bin/git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+ln -s ~/.fzf ~/.vim/bundle/pristine/fzf
+/usr/bin/git clone https://github.com/junegunn/fzf.vim.git ~/.vim/bundle/pristine/fzf.vim
 ln -s ~/.dotfiles/custom_config.vim ~/.vim
